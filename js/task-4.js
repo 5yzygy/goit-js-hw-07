7937dee6d17300ed46e31f5cd931cd428592badc
5yzygy <aemre.harsa@gmail.com>
2025-02-20 23:33:29 +0300

    Task 4 completed

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) =>
+{
+    event.preventDefault();
+
+    const inputs = form.elements;
+    const forms = {};
+
+    for (let input of inputs)
+    {
+        if (input.name && input.value.trim() === "")
+        {
+            alert("All form fields must be filled in.");
+            return;
+        }
+        
+        else if (input.name)
+        {
+            forms[input.name] = input.value.trim();
+        }
+    }
+    
+    console.log(forms);
+    form.reset();
+});
