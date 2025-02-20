3bdfa1bf1a857020de37f3cc019829fb932bb34a
5yzygy <aemre.harsa@gmail.com>
2025-02-20 23:45:44 +0300

    Task 5 completed

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+let button = document.querySelector(".change-color");
+
+button.addEventListener("click", () =>
+{
+    const colourSpan = document.querySelector(".color");
+    
+    function getRandomHexColor()
+    {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    }
+  
+    document.body.style.backgroundColor = getRandomHexColor();
+    colourSpan.textContent = getRandomHexColor();
+});
+
